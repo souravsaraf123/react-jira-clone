d70f67789bc1e2bf9b35aec86bd6e764f8f54bd3
Sourav Saraf <souravs@2pirad.com>
2024-03-23 18:08:43 +0530

    refactor project structure, added css resets and helpers, added fonts, developed sidebar component

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './assets/styles/fonts.css';
+import './assets/styles/variables.css';
+import './assets/styles/reset.css';
+import './assets/styles/index.css';
+
+import App from './App.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+);
