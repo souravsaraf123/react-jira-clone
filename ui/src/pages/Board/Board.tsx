f284d166a3fb310eb96257015bb65e9257cc2cf0
Sourav Saraf <souravs@2pirad.com>
2024-04-06 13:23:35 +0530

    wip

--- ui/src/pages/Board/Board.tsx
+++ ui/src/pages/Board/Board.tsx
@@ -1,9 +1,46 @@
+import "./Board.css";
+
+import { Breadcrumb } from "../../shared/components/Breadcrumb/Breadcrumb";
+import { GithubLink } from "../../shared/GithubLink/GithubLink";
+import { ProjectContextType } from "../../App";
+import { useOutletContext } from "react-router-dom";
+
 export function Board()
 {
+	let [project, setProject]: any = useOutletContext<ProjectContextType>();
 	return (
-		<div>
-			<h1>Kanban Board</h1>
-			{/* Add your component code here */}
+		<div className="board_container">
+
+			{/* Header Container */}
+			<div>
+
+				{/* Breadcrumb */}
+				<Breadcrumb project={project} lastPage="Kanban Board" />
+
+				{/* Title Container */}
+				<div style={{ display: "flex", justifyContent: "space-between" }}>
+
+					{/* Title */}
+					<span className="title">Kanban Board</span>
+
+					{/* Github Link */}
+					<GithubLink />
+
+				</div>
+
+			</div>
+
+			{/* Search & Filter Container */}
+			<div className="search_filter_container">
+
+				{/* Search Input */}
+				<input
+					className="form_input search_input"
+					placeholder="Search Issue Title"
+					type="search" />
+
+			</div>
+
 		</div>
 	);
 }
